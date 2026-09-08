f66e12028b06e6015b84b87a5487eb97e4e454a3
SrijanKumarSinha <srijansinha9007@gamil.com>
2026-09-08 20:40:51 +0530

    Initial commit with folder structure and READMEs

---
+++ Python/PythonReconstructBegins2026.py
@@ -0,0 +1,24 @@
+# ex-2.1 
+x = "Hello World!"
+print(x)
+
+# ex-2.2
+y = "Hello_World_!"
+print(y)
+
+y = "HelloWorld!"
+print(y)
+
+# ex-2.3
+name = "Xavier"
+print(f"Hello {name} ,would you like to learn python today ?")
+
+# ex-2.4
+name = "Sita Ram"
+print(name.upper())
+print(name.lower())
+print(name.title())
+
+# ex-2.5
+Quote = "If you hit the bottom doesn't mean that you have to be there"
+print(f"Robert Downey Jr. once said : ", Quote)
