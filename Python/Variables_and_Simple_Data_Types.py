0c7ef842621345943e32492fef888a0b6b77dae0
SrijanKumarSinha <srijansinha9007@gamil.com>
2026-09-09 12:54:10 +0530

    Updated Exercises Problem

---
+++ Python/Variables_and_Simple_Data_Types.py
@@ -0,0 +1,47 @@
+print("ex-2.1") 
+x = "Hello World!"
+print(x)
+
+print("ex-2.2")
+y = "Hello_World_!"
+print(y)
+
+y = "HelloWorld!"
+print(y)
+
+print("ex-2.3")
+name = "Xavier"
+print(f"Hello {name} ,would you like to learn python today ?")
+
+print("ex-2.4")
+name = "Sita Ram"
+print(name.upper())
+print(name.lower())
+print(name.title())
+
+print("ex-2.5")
+
+print(f'Robert Downey Jr. once said , "If you hit the bottom, doesn\'t mean that you have to be there"')
+
+print("ex-2.6")
+
+famous_person = "Tony Stark"
+message = f'{famous_person} once said, "I am Iron Man."'
+print(message)
+
+
+print("ex-2.7")
+
+name = "\tJohn Doe\n"
+print(name)
+print(name.lstrip())
+print(name.rstrip())
+print(name.strip())
+
+print("ex-2.8")
+
+filename = "python_notes.txt"
+clean_name = filename.removesuffix(".txt")
+
+
+print(clean_name)
