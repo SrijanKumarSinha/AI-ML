b1b284e74509a75c9a3260a2bbdbb5130c86314a
Srijan Kumar Sinha <320100762+SrijanKumarSinha@users.noreply.github.com>
2026-09-10 19:47:13 +0530

    Working With Lists

---
+++ Python/Working_With_Lists.py
@@ -0,0 +1,24 @@
+print("\nex-4.1 == Pizzas\n")
+
+favourite_food = ['Samosa','Bhujia','Kachodi']
+for food in favourite_food:
+    print(f"I like {food.title()}")
+print(f"I like {favourite_food[0]},{favourite_food[1]},{favourite_food[2]}")
+print("I love these foods")
+
+print("\nex-4.2 == Animals\n")
+
+animals = ['Lion','Tiger','Wolf']
+for animal in animals:
+    print(animal)
+print(f"{animals[0]} is the king of the jungle.")
+print(f"{animals[1]} is the national animal of india.")
+print(f"{animals[2]} is a clever animal.")
+
+print("\nAll the animals are carnivorous\n")
+
+print("\nex-4.3 == Counting To Twenty")
+
+
+
+ 
