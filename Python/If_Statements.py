cf26b468999083924c726893c2d66cb910803b17
Srijan Kumar Sinha <320100762+SrijanKumarSinha@users.noreply.github.com>
2026-09-12 14:18:58 +0530

    If_Statements

---
+++ Python/If_Statements.py
@@ -0,0 +1,55 @@
+print("ex-5.1 == Condititonal Tests\n")
+#1
+car = 'bmw'
+print("Is car == 'bmw' ? I predict True.")
+print(car == 'bmw')
+
+print("Is car == 'mercedes' ? I predict False.")
+print(car == 'mercedes')
+#2
+car = 'audi'
+print("\nIs car == 'audi' ? I predict True.")
+print(car == 'audi')
+
+print("Is car == 'toyota' ? I predict False")
+print(car == 'toyota')
+#3
+number = 2
+print("\nIs number == 2 ? I predict True.")
+print(number == 2)
+
+print("Is number == 3 ? I predict False.")
+print(number == 3)
+#4
+strongest_avengers = 'Thor'
+print("\nIs strongest_avenger == 'Thor' ? I predict True.")
+print(strongest_avengers == 'Thor')
+
+print("Is strongest_avengers == 'Hawkeye' ? I predict False.")
+print(strongest_avengers == 'Hawkeye')
+#5
+number = 7
+print("\nIs number == 7 ? I predict True.")
+print(number == 7)
+
+print("Is number == 8 ? I predict False.")
+print(number == 8)
+
+print("\nex-5.2 == More Conditional Tests")
+
+number = 7
+
+if number == 7:
+    print("True")
+ 
+
+list = ['bmw','toyota','subaru','mercedes']
+
+if 'bmw' in list:
+    print("Item is in the list")
+elif 'tata' in list:
+    print("Item is not in the list")
+
+
+
+
