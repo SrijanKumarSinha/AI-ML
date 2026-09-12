a2f975c4ef63d6f1aa70ea4ceda79c619ee1e24a
Srijan Kumar Sinha <320100762+SrijanKumarSinha@users.noreply.github.com>
2026-09-12 19:12:31 +0530

    Added more lines of code

--- Python/If_Statements.py
+++ Python/If_Statements.py
@@ -46,10 +46,107 @@ if number == 7:
 list = ['bmw','toyota','subaru','mercedes']
 
 if 'bmw' in list:
-    print("Item is in the list")
+    print("Item is in the list..")
 elif 'tata' in list:
     print("Item is not in the list")
 
+print("\nex-5.3 == Alien Colour #1")
+
+alien_colour = 'green'
+if alien_colour is 'green':
+    print("You earned 5 points")
+else:
+    print("Earned 0 points")
+
+print("\nex-5.4 == Alien Colour #2")
+alien_colour = 'green'
+if alien_colour is 'green':
+    print("You just earned 5 points for shooting the alien.")
+else:
+    print("Player just earned 10 points.")
+
+print("\n#2")
+alien_colour = 'red'
+if alien_colour is 'green':
+    print("You just earned 5 points for shooting the alien.")
+else:
+    print("Player just earned 10 points.")
+
+print("\nex-5.5 == Alien Colors #3") # 3 versions for if, elif and else statements
+
+alien_colour = 'green' # for if statements
+
+if alien_colour is 'green':
+    print("Player earned 5 points")
+elif alien_colour is 'yellow':
+    print("Player earned 10 points")
+else:
+    print("Player earned 20 points")
+
+alien_colour = 'yellow' # for elif statement
+
+if alien_colour is 'green':
+    print("Player earned 5 points")
+elif alien_colour is 'yellow':
+    print("Player earned 10 points")
+else:
+    print("Player earned 20 points")
+
+alien_colour = 'red' # for else statement
+
+if alien_colour is 'green':
+    print("Player earned 5 points")
+elif alien_colour is 'yellow':
+    print("Player earned 10 points")
+else:
+    print("Player earned 20 points")
+
+print("\nex-5.6 == Stages of life")
+
+age = 25
+
+if age < 2 :
+    print("The person is a baby.")
+elif age < 4:
+    print("The person is a toddler.")
+elif age < 13:
+    print("The person is a kid.")
+elif age < 20:
+    print("The person is a teenager.")
+elif age < 65:
+    print("The person is an adult.")
+else:
+    print("The person is an elder.")
 
 
+print("\nex-5.7 == Favourite foods")
 
+favorite_fruits = ['mango','apple','guava']
+
+if 'mango' in favorite_fruits:
+    print(f"I like {favorite_fruits[0]}")
+elif 'apple' in favorite_fruits:
+    print(f"I like {favorite_fruits[1]}")
+elif 'guava' in favorite_fruits:
+    print(f"I like {favorite_fruits[2]}")
+elif 'pineapple' in favorite_fruits:
+    print("Item not found")
+elif "orange" in favorite_fruits:
+    print("Item is not found")
+
+print("\nex-5.8 == Hello Admin")
+
+usernames = ['admin01','admin02','admin03','admin04','admin05']
+for username in usernames:
+    print(f"Hello {username.title()} , Thankyou for logging in again")
+
+print("\nex-5.9 == No users")
+usernames = []
+for username in usernames:
+    print(username)
+print("We need to find some users")
+
+print("\nex-5.10 == Checking Username")
+
+
+ 
