b4b73e79df41bcbbe27d998fbfbce9361d661bb0
Srijan Kumar Sinha <320100762+SrijanKumarSinha@users.noreply.github.com>
2026-09-13 21:25:38 +0530

    Dictionaries

---
+++ Python/Dictionaries.py
@@ -0,0 +1,27 @@
+print("\nex-6.1 == Persons")
+
+user_information = { 
+    'first_name': 'Tony',
+    'last_name': 'Stark',
+    'age': 48,
+    'city': 'New York'
+    }
+print(user_information)
+print(user_information['first_name'])
+print(user_information['last_name'])
+print(user_information['age'])
+print(user_information['city'])
+
+print("\nex-6.2 == Favorite Numbers")
+
+favorite_numbers = {
+    'Tony stark' : '7',
+    'Steve rogers' : '2',
+    'Thor' : '5',
+    'Clint barton' : '3',
+    'Bruce banner' : '4',
+    'Natasha romanoff' : '8'
+    }
+
+print(favorite_numbers)
+print(favorite_numbers['Tony stark'])
