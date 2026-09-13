141d5389a1edc6a31de6684e1ebfb26ff1540a61
Srijan Kumar Sinha <320100762+SrijanKumarSinha@users.noreply.github.com>
2026-09-13 23:35:46 +0530

    Added more lines of code

--- Python/Dictionaries.py
+++ Python/Dictionaries.py
@@ -24,4 +24,27 @@ favorite_numbers = {
     }
 
 print(favorite_numbers)
-print(favorite_numbers['Tony stark'])
+print(f"Tony Stark's favorite number is {favorite_numbers['Tony stark']}.")
+print(f"Steve Rogers' favorite number is {favorite_numbers['Steve rogers']}.")
+print(f"Thor's favorite number is {favorite_numbers['Thor']}.")
+print(f"Clint Barton's favorite number is {favorite_numbers['Clint barton']}.")
+print(f"Bruce Banner's favorite number is {favorite_numbers['Bruce banner']}.")
+print(f"Natasha Romanoff's favorite number is {favorite_numbers['Natasha romanoff']}.")
+
+print("\nex-6.3 == Glossary")
+
+glossary = {
+    'list' : 'A collection of items in a particular order.',
+    'tuple' : 'A collection of items that is ordered and unchangeable.',
+    'dictionary' : 'A collection of key-value pairs.',
+    'set' : 'A collection of unique items.',
+    'string' : 'A sequence of characters.'
+    }
+
+print(f"List : {glossary['list']}")
+print(f"\nTuple : {glossary['tuple']}")
+print(f"\nDictionary : {glossary['dictionary']}")
+print(f"\nSet : {glossary['set']}")
+print(f"\nString : {glossary['string']}")
+
+
