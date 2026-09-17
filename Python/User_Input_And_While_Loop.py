4e7581aec8c484c771d2d9a1290160f93e029705
Srijan Kumar Sinha <320100762+SrijanKumarSinha@users.noreply.github.com>
2026-09-17 17:01:07 +0530

    Added more lines of code

--- Python/User_Input_And_While_Loop.py
+++ Python/User_Input_And_While_Loop.py
@@ -65,6 +65,62 @@ while x <= 5:
     print(x)
     x += 1   # for infinity loop remove this line and run the code. It will run forever.
 
+print("\nex- 7.8 == Deli")
+
+sandwich_orders = ['veg sandwich', 'non-veg sandwich','tuna sandwich','pastrami','pastrami','pastrami']
+finished_sandwiches =[]
+while sandwich_orders:
+    sandwiches = sandwich_orders.pop()
+
+    print(f"I made your {sandwiches.title()}")
+    finished_sandwiches.append(sandwiches)
+
+print("\n---Sandwiches were Made ---")
+for finished_sandwich in finished_sandwiches:
+    print(finished_sandwich.title())
+
+print("\nex- 7.9 == No Pastrami")
+
+sandwich_orders = ['veg sandwich', 'non-veg sandwich','tuna sandwich','pastrami','pastrami','pastrami']
+finished_sandwiches =[]
+
+print("Sorry, the deli has run out of pastrami today!\n")
+
+while 'pastrami' in sandwich_orders:
+    sandwich_orders.remove('pastrami')
+while sandwich_orders:
+    sandwiches = sandwich_orders.pop()
+
+    print(f"I made your {sandwiches.title()}")
+    finished_sandwiches.append(sandwiches)
+
+print("\n---Sandwiches were Made ---")
+for finished_sandwich in finished_sandwiches:
+    print(finished_sandwich.title())
+
+print("\nex- 7.10 == Dream Vacation")
+
+responses = {}
+polling_active = True
+
+while polling_active:
+    name = input("\nWhat is your name? ")
+    response = input("If you could visit one place in the world, where would you go? ")
+
+    responses[name] = response
+
+    repeat = input("Would you like to let another person respond? (yes/no) ")
+    if repeat == 'no':
+        polling_active = False
+
+print("\n ")
+print("\t--- Poll Results ---")
+for name, place in responses.items():
+    print(f"{name.title()} would like to visit {place.title()}.\n")
+
+print("\t--- End of the Code ---")
 
 
-    
+
+ 
+ 
