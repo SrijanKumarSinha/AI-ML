664d6baed86a7cfe6f441ab888e19f536830a42b
Srijan Kumar Sinha <320100762+SrijanKumarSinha@users.noreply.github.com>
2026-09-17 15:48:02 +0530

    User_Input_And_Loop

---
+++ Python/User_Input_And_While_Loop.py
@@ -0,0 +1,47 @@
+print("ex- 7.1 == Rental Car")
+
+car_name = input("Enter your car name: ")
+print(f"\nLet me see if i can find you a {car_name}.")
+
+print("\nex- 7.2 == Restaurant Setting")
+people_number = input("How many people are there in their dinner group: ")
+people_number = int(people_number)
+
+if people_number > 8:
+    print("\nWait for your table")
+else:
+    print("\nYour table is ready")
+
+print("\nex- 7.3 == Multiples of ten")
+
+print("\tEnter your number to know if it is a multiple of 10 or not\n")
+number = input("Enter your number: ")
+number = int(number)
+
+if number % 10 == 0:
+    print(f"\tThe number {number} is multiple of 10.")
+else:
+    print(f"\tThe number {number} is not a multiple of 10")
+
+print("\nex- 7.4 == Pizza Toppings")
+
+toppings = "\nEnter your toppings name for your pizza: "
+toppings += "\nEnter 'quit' to end the program."
+
+message = " "
+while message != 'quit':
+    message = input(toppings)
+    print(message)
+
+print("\nex- 7.5 == Movie Tickets")
+
+age = input("Enter your age to know your movie ticket price: ")
+age = int(age)
+
+if age < 3:
+    print("The movie ticket is free")
+elif age >= 3 and age <= 12:
+    print("The movie ticket price is 10 ruppes only")
+else:
+    print("The movie ticket price is 15 ruppes only")
+
