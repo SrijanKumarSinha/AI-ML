3d383f714b172c2acbfbe3be051fa53c870ad9a4
Srijan Kumar Sinha <320100762+SrijanKumarSinha@users.noreply.github.com>
2026-09-18 08:19:44 +0530

    Functions

---
+++ Python/Functions.py
@@ -0,0 +1,13 @@
+print("ex- 8.1 == Message")
+
+def display_message():
+    '''Display a sentence.'''
+    print("I am learning today Functions.")
+
+display_message()
+
+print("\nex- 8.2 == Favorite Book")
+
+def favorite_book(Bookname):
+    print(f"Currently my favorite book is {Bookname.title()}.")
+favorite_book('Python Crash Course by Eric Matthes')
