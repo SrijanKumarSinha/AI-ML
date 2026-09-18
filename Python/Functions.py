0b5f09954c641285d96396cc6542d1d3e0e5af07
Srijan Kumar Sinha <320100762+SrijanKumarSinha@users.noreply.github.com>
2026-09-18 20:28:38 +0530

    Added more lines of code

--- Python/Functions.py
+++ Python/Functions.py
@@ -11,3 +11,32 @@ print("\nex- 8.2 == Favorite Book")
 def favorite_book(Bookname):
     print(f"Currently my favorite book is {Bookname.title()}.")
 favorite_book('Python Crash Course by Eric Matthes')
+
+print("\nex- 8.3 == T-Shirt")
+
+def make_shirt(size, message):
+    print(f"\nThe size of the T-shirt is {size} and the message printed on it is '{message}'.")
+
+make_shirt('Large', 'I love Python')
+make_shirt('Medium', 'Python is awesome!')
+make_shirt('Small', 'Code with Python')
+
+print("\nex- 8.4 == Large Shirts")
+
+def make_shirt(size='Large', message='I love Python'):
+    print(f"\nThe size of the T-shirt is {size} and the message printed on it is '{message}'.")
+
+make_shirt('Medium', 'Python is awesome!')
+make_shirt('Double Extra Large', 'I am Iron Man')
+make_shirt()  # Using default values
+
+print("\nex- 8.5 == Cities")
+
+def describe_city(city, country='INDIA'):
+    print(f"{city.title()} is in {country.title()}.")
+
+describe_city('Delhi')
+describe_city('Mumbai')
+describe_city('Jharkhand')
+describe_city('Bihar')
+
