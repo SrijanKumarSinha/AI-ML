7710933cec0e5f5f49490d50bd6bb1b5d8746e11
Srijan Kumar Sinha <320100762+SrijanKumarSinha@users.noreply.github.com>
2026-09-21 21:39:08 +0530

    Classes

---
+++ Python/Classes.py
@@ -0,0 +1,10 @@
+print("9.1 == Restaurant")
+
+def describe_restaurant(restaurant_name, cuisine_type):
+    print(f"{restaurant_name.title()} serves {cuisine_type.title()} cuisine.")
+
+describe_restaurant("pizza place", "Italian")
+describe_restaurant("sushi bar", "Japanese")
+describe_restaurant("burger joint", "American")
+describe_restaurant("dal - chawal", "Indian")
+
